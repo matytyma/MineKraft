bdbd5dcd2f472ee3eb82c18e0d1eb53b2c43ff6a
matytyma <matytyma22@gmail.com>
2024-08-14 13:46:08 +0200

    Progress on `ItemMetaBuilder`

--- src/main/kotlin/inventory/meta/ItemMetaBuilder.kt
+++ src/main/kotlin/inventory/meta/ItemMetaBuilder.kt
@@ -4,7 +4,9 @@ import dev.matytyma.minekraft.inventory.ItemStackBuilder
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.meta.ItemMeta
+import sun.jvm.hotspot.oops.CellTypeState.value
 
 @JvmInline
 value class ItemMetaBuilder(internal val meta: ItemMeta) {
@@ -12,11 +14,63 @@ value class ItemMetaBuilder(internal val meta: ItemMeta) {
         get() = meta.displayName()
         set(value) = meta.displayName(value)
 
-    // TODO: Check if has item name
-    var itemName: Component
-        get() = meta.itemName()
+    var itemName: Component?
+        get() = if (meta.hasItemName()) meta.itemName() else null
         set(value) = meta.itemName(value)
 
+    var lore: List<Component?>?
+        get() = meta.lore()
+        set(value) = meta.lore(value)
+
+    var customModelData: Int?
+        get() = if (meta.hasCustomModelData()) meta.customModelData else null
+        set(value) = meta.setCustomModelData(value)
+
+    var enchantments: MutableMap<Enchantment, Int>
+        get() = object : MutableMap<Enchantment, Int> {
+            // region Enchantments
+            override val entries: MutableSet<MutableMap.MutableEntry<Enchantment, Int>>
+                get() = meta.enchants.entries
+
+            override val keys: MutableSet<Enchantment>
+                get() = meta.enchants.keys
+
+            override val size: Int
+                get() = meta.enchants.size
+
+            override val values: MutableCollection<Int>
+                get() = meta.enchants.values
+
+            override fun clear() = meta.removeEnchantments()
+
+            override fun isEmpty(): Boolean = meta.hasEnchants()
+
+            override fun remove(key: Enchantment): Int? {
+                val previous = meta.getEnchantLevel(key)
+                meta.removeEnchant(key)
+                return if (previous == 0) null else previous
+            }
+
+            override fun putAll(from: Map<out Enchantment, Int>) =
+                from.forEach { meta.addEnchant(it.key, it.value, false) }
+
+            override fun put(key: Enchantment, value: Int): Int? {
+                val previous = meta.getEnchantLevel(key)
+                meta.addEnchant(key, value, false)
+                return if (previous == 0) null else previous
+            }
+
+            override fun get(key: Enchantment): Int? = meta.getEnchantLevel(key).let { if (it == 0) null else it }
+
+            override fun containsValue(value: Int): Boolean = throw UnsupportedOperationException("")
+
+            override fun containsKey(key: Enchantment): Boolean = meta.hasEnchant(key)
+            // endregion
+        }
+        set(value) {
+            enchantments.clear()
+            enchantments.putAll(value)
+        }
 }
 
 fun itemMeta(
