52498eedbf2f9f1bf4eee6b079a6d611719c31f6
matytyma <matytyma22@gmail.com>
2024-08-15 19:32:10 +0200

    Fix some types in `MutableMultiMap`

--- src/main/kotlin/collection/MultiMap.kt
+++ src/main/kotlin/collection/MultiMap.kt
@@ -7,7 +7,7 @@ interface MutableMultiMap<K, V> {
 
     operator fun set(key: K, value: V)
 
-    operator fun set(key: K, values: List<V>)
+    operator fun set(key: K, values: Collection<V>)
 
     fun putAll(from: Map<out K, Collection<V>>)
 
@@ -17,7 +17,7 @@ interface MutableMultiMap<K, V> {
 
     fun addAll(from  : Map<out K, Collection<V>>): Boolean
 
-    fun remove(key: K): List<V>?
+    fun remove(key: K): Boolean
 
     operator fun plusAssign(pair: Pair<K, V>)  {
         add(pair.first, pair.second)
