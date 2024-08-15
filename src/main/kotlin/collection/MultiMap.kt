a1371c3b20da6138d157655ff82a7da4a8bb0564
matytyma <matytyma22@gmail.com>
2024-08-15 18:25:52 +0200

    Create `MutableMultiMap` interface

---
+++ src/main/kotlin/collection/MultiMap.kt
@@ -0,0 +1,35 @@
+package dev.matytyma.minekraft.collection
+
+interface MutableMultiMap<K, V> {
+    fun isEmpty(): Boolean
+
+    operator fun get(key: K): List<V>?
+
+    operator fun set(key: K, value: V)
+
+    operator fun set(key: K, values: List<V>)
+
+    fun putAll(from: Map<out K, Collection<V>>)
+
+    fun add(key: K, value: V): Boolean
+
+    fun add(key: K, values: Collection<V>): Boolean
+
+    fun addAll(from  : Map<out K, Collection<V>>): Boolean
+
+    fun remove(key: K): List<V>?
+
+    operator fun plusAssign(pair: Pair<K, V>)  {
+        add(pair.first, pair.second)
+    }
+
+    operator fun plusAssign(pair: Pair<K, Collection<V>>) {
+        add(pair.first, pair.second)
+    }
+
+    operator fun minusAssign(key: K) {
+        remove(key)
+    }
+
+    operator fun minusAssign(keys: Collection<K>) = keys.forEach { remove(it) }
+}
