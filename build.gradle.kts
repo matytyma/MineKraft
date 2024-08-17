f4f173940d99af94db7195a6baa37d62b6546960
matytyma <matytyma22@gmail.com>
2024-08-17 14:01:45 +0200

    Add Brigadier dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://libraries.minecraft.net")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("com.mojang:brigadier:1.0.18")
 }
 
 kotlin {
