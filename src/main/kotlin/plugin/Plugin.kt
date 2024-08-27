dcc422de1bad4a3695d47203222519f3d932b1dc
matytyma <matytyma22@gmail.com>
2024-08-27 12:39:21 +0200

    Inlining

--- src/main/kotlin/plugin/Plugin.kt
+++ src/main/kotlin/plugin/Plugin.kt
@@ -6,14 +6,14 @@ import org.bukkit.plugin.Plugin
 inline fun <reified T : Event> Plugin.on(
     priority: EventPriority = EventPriority.NORMAL,
     ignoreCancelled: Boolean = false,
-    noinline handler: T.() -> Unit,
+    crossinline handler: T.() -> Unit,
 ): Listener = on(T::class.java, priority, ignoreCancelled, handler)
 
-fun <T : Event> Plugin.on(
+inline fun <T : Event> Plugin.on(
     eventClass: Class<out T>,
     priority: EventPriority = EventPriority.NORMAL,
     ignoreCancelled: Boolean = false,
-    handler: T.() -> Unit,
+    crossinline handler: T.() -> Unit,
 ): Listener {
     val listener: Listener = object : Listener {}
     val executor: (Listener, Event) -> Unit = { _, event ->
